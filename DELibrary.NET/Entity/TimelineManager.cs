3d298762db2f786be059bb4ceff8443baa4cc45b
Jhrino <orhankaymak@windowslive.com>
2024-11-19 18:28:41 +0300

    Update TimelineManager.cs

--- DELibrary.NET/Entity/TimelineManager.cs
+++ DELibrary.NET/Entity/TimelineManager.cs
@@ -12,5 +12,8 @@ namespace DragonEngineLibrary
         [DllImport("Y7Internal.dll", EntryPoint = "LIB_CTIMELINE_MANAGER_CHECK_CLOCK_ACHIEVEMENT", CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.U1)]
         public static extern bool CheckClockAchievement(uint timeline_id, uint sheet_id, uint clock_id);
+
+        [DllImport("Y7Internal.dll", EntryPoint = "LIB_CTIMELINE_MANAGER_GET_CURRENT_CLOCK", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetCurrentClock(uint timeline_id, uint sheet_id);
     }
 }
