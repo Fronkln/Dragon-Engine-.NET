951cee332493638e82cd2bbbc04f97066120bd5d
James <plyrthn@users.noreply.github.com>
2026-03-19 11:19:26 -0400

    Monolithic cimgui.dll: DX11/DX12 hook + imgui 1.92.4 + all addons
    Vendor deps/ containing:
    - cimgui (imgui 1.92.4-docking + C wrapper)
    - dxhook (DX11/DX12 Present hook + threaded dispatch)
    - implot, imnodes, cimguizmo, cimplot3d
    - minhook (for vtable hooking)
    - prebuilt Hexa.NET managed bindings
    
    Remove old ImGui.NET-master vendored source (replaced by Hexa.NET).
    Remove .gitmodules (deps are vendored, not submodules).
    Add CI workflow for all game configs.

---
+++ deps/cimplot3d/generator/cimplot3d_template.h
@@ -0,0 +1,20 @@
+#ifndef CIMPLOT3D_INCLUDED
+#define CIMPLOT3D_INCLUDED
+
+#include "cimgui.h"
+
+#ifdef CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "imgui_structs.h"
+#else
+#endif // CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+
+PLACE_STRUCTS_C
+
+#include "auto_funcs.h"
+
+
+#endif //CIMPLOT3D_INCLUDED
+
+
+
+
